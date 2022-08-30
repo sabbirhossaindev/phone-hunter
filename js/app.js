6d6d1201be50d6930064ae016701ccaa63281cfc
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-30 23:59:42 +0600

    done

--- js/app.js
+++ js/app.js
@@ -8,6 +8,19 @@ const loadPhones = async (searchText) => {
 const displayPhones = phones => {
     const phoneContainer = document.getElementById('phone-container');
     phoneContainer.textContent = '';
+    // display 20 Phone only.....
+    phones = phones.slice(0, 20);
+
+    // display no found message
+    const noPhone = document.getElementById('no-found-message');
+    if(phones.length === 0){
+        noPhone.classList.remove('d-none');
+    }
+    else{
+        noPhone.classList.add('d-none');
+    }
+
+    // display all phone
     phones.forEach(phone => {
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
